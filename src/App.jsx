9d7f3619845f6a81c75acaf99ab2a63e0ab74544
Andrey Yasnovsky <yasnovsky@Andreys-MacBook-Pro.local>
2022-12-23 23:08:49 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
+import { Modal } from '@tourmalinecore/react-tc-modal';
+import { Button, NativeSelect } from '@tourmalinecore/react-tc-ui-kit';
 import clsx from 'clsx';
 import { useEffect, useState } from 'react';
 import './App.css';
-import InfoImage from './components/InfoImage/InfoImage';
+import Loading from './components/Loading/Loading';
 
 function App() {
   const [image, setImage] = useState({});
   const [newImage, setNewImage] = useState();
-  const [tags, setTags] = useState({
-    hash: '',
-    emotions: '',
-    photo_color: [],
-    objects: [],
+  const [tags, setTags] = useState();
+  const [option, setOption] = useState({
+    label: 1,
+    value: 1,
   });
 
   const [isLoading, setIsLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isTrue, setTrue] = useState(false);
 
 
   useEffect(() => {
@@ -28,11 +31,15 @@ function App() {
 
     if (Object.keys(image).length < 1) return;
 
-     downloadRR(image);
+     sendImage(image);
 
   },[image]);
 
-  console.log(tags);
+
+  const arrayNumber = [...Array(10)].map((e, i) => i + 1);
+
+
+  const waitResult =  isLoading ? <Loading /> : 'Тут будет результат';
 
   return (
     <div className="app">
@@ -41,82 +48,123 @@ function App() {
         'app__photo-input-view--download':newImage
       })}>
 
-        {/* <div>
-          <input className='app__photo-input' type="file" onChange={download} />
-        </div> */}
-
-
           {newImage ? (<img className='app__photo-view' src={newImage} alt="foto" />) : (
-            <div style={{
-              minHeight: 400,
-              maxHeight: 500,
-              backgroundColor: 'red',
-              borderRadius: 10,
-            }}>
+            <div style={{ minHeight: 400, maxHeight: 500, backgroundColor: 'red', borderRadius: 10 }}>
                 превью
             </div>
           )}
 
 
         <div className='app__photo-input-div'>
-          <div style={{
-              position: 'absolute',
-              top:'50%',
-              left: '50%',
-              pointerEvents: 'none',
-              transform: 'translate(-50%, -50%)',
-            }}>
+          <div style={{position: 'absolute', top:'50%', left: '50%', pointerEvents: 'none', transform: 'translate(-50%, -50%)'}}>
             Choose file...
             </div>
-          <input className='app__photo-input' type="file" onChange={download} />
+          <input className='app__photo-input' type="file" onChange={getInputImage} />
         </div>
       </div>
 
+        <div>
+            <div style={{width: 250, height: 250, fontSize: 50, textAlign: 'center'}}>{tags ? tags: waitResult }</div>
+            <div style={{ display: 'flex'}}>
+        <Button 
+          color="primary"
+          onClick={() => {
+            setTrue(true);
+          }}
+          disabled={!newImage}
+        >Верно</Button>
+        <Button 
+          color="danger"
+          onClick={() => {
+            setIsOpen(true);
+          }}
+          disabled={!newImage}
+        >Неверно</Button>
+</div>
+        </div>
 
-{Object.keys(image).length < 1  ? (<div>Upload image</div>) : (
-    <InfoImage 
-      isLoading={isLoading} 
-      tags={tags}
-    />)}
-      
+
+        {isTrue && (
+        <Modal 
+          title="Ура, чет умное сделали"
+          content="Данные сохранили в бекенде! Беги смотреть"
+          cancelText="Cancel"
+          onClose={() => {
+            setTrue(false);
+            sendApruvResult(image, tags);
+
+            // setTags();
+            setNewImage();
+            // setImage();
+            setImage(Object.create({}));
+            console.log(image);
+            console.log(Object.create({}));
+          }}
+        />)}
+    
+
+        {isOpen &&(
+        <Modal 
+          title="Эх, чет надо думать, а сейчас помогай"
+          content={(
+            <NativeSelect 
+              value={option.value}
+              options={arrayNumber.map(i => ({
+                label: i,
+                value: i
+              }))}
+              onChange={(e) => setOption(e)}
+            />
+          )}
+          cancelText="Отправить"
+          onClose={() => {
+            setIsOpen(false);
+          }}
+          showCancel
+          onCancel={() => {
+            sendApruvResult(image, option.value);
+            setTags();
+            setNewImage();
+            setImage(Object.create({}));
+          }}
+        />
+        )}
     </div>
   );
 
-  async function  downloadRR(image) {
+  async function sendImage(image) {
 
     setIsLoading(true);
 
-    const respos = await fetch("http://localhost:7501/api/photos/add", {
+    const respos = await fetch("http://localhost:9998/image/", {
       body: image[0],
       headers:{
-        'Access-Control-Allow-Origin': 'http://localhost:3000/',
+        'Access-Control-Allow-Origin': '*',
         'Access-Control-Request-Method': 'POST'
       },
       method: "POST"
     });
 
-    const {photo_id}  = await respos.json();
+    const {answer}  = await respos.json();
 
-    // console.log(photo_id);
-    // setId(photo_id);
-
-    const responsTags = await fetch(`http://localhost:7501/api/labels/${photo_id}`, {
-      headers:{
-        'Access-Control-Allow-Origin': 'http://localhost:3000/',
-        'Access-Control-Request-Method': 'POST'
-      },
-      method: "GET"
-    })
-  
-    const data = await responsTags.json();
-
-    if (data) {
-      setTags(data);
-      setIsLoading(false);
-     }
+    setTags(answer);
+    setIsLoading(false);
   }
 
-  function  download(event) {
+  async function  sendApruvResult(image, number) {
+
+    await fetch(`http://localhost:9998/image/save/${number}`, {
+      body: image[0],
+      headers:{
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Request-Method': 'POST'
+      },
+      method: "POST"
+    })
+  
+  }
+
+  function  getInputImage(event) {
     setImage(event.target.files);
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@tourmalinecore/react-tc-modal/es/index.css';
+import '@tourmalinecore/react-tc-ui-kit/es/index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
